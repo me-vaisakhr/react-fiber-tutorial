8428015357f5f07ff26202df89e7c272248dfb3b
vaisakh.krishnan <vaisakh.krishnan@entri.me>
2023-03-24 10:09:55 +0530

    updated the vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:`/react-fiber-tutorial`,
-  assetsInclude: ['**/*.gltf']
-})
+  base: `/react-fiber-tutorial`,
+  assetsInclude: ["**/*.gltf"],
+  root: "./",
+  build: {
+    outDir: "dist",
+  },
+  publicDir: "assets",
+});
